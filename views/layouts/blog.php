12ec58374f740556dbbe11a6f9333629e298c81b
WasTeime <miracleup10@gmail.com>
2024-08-22 16:05:04 +0300

    Вынес подключение ресурсов из представления в assets.

--- views/layouts/blog.php
+++ views/layouts/blog.php
@@ -6,16 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Front page</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
+    <title>Golovkin Practice</title>
 </head>
 
 <body>
@@ -205,11 +196,6 @@
             </div>
         </footer>
     </div>
-    <script src="js/jquery-3.1.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/active.js"></script>
-    <script src="js/isotope.pkgd.js"></script>
 </body>
 
 </html>
