26fb98844e44ecf02f45abae7ce8518ceda10e85
Sergey <emelyanov.sm2002@gmail.com>
2024-08-23 16:40:29 +0300

    Добавил предложенные исправления IDE

--- models/Post.php
+++ models/Post.php
@@ -3,14 +3,15 @@
 namespace app\models;
 
 use yii\db\ActiveRecord;
+use yii\helpers\Inflector;
 
 class Post extends ActiveRecord
 {
-    public static function tableName()
+    public static function tableName(): string
     {
         return 'post';
     }
-    public function getCategory()
+    public function getCategory(): \yii\db\ActiveQuery
     {
         return $this->hasOne(Category::class, ['id' => 'category_id']);
     }
